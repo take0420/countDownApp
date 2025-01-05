4af706bc4f9673d03a2c5e405600a1c919c9f3e7
take0420 <zakisen81@gmail.com>
2025-01-05 11:24:54 +0900

    feat:

--- main.js
+++ main.js
@@ -3,4 +3,9 @@
 {
   const timer = document.getElementById('timer');
   const btn = document.getElementById('btn');
+
+  // タイマーの終了時刻を求める
+  btn.addEventListener('click', () => {
+    const endTime = new Date().getTime() + 3 * 1000;
+  });
 }
