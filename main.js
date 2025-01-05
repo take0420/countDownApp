7196e77e1af79415c8ae8617c171f0445da8fb1f
take0420 <zakisen81@gmail.com>
2025-01-05 11:32:13 +0900

    feat: タイマー終了後に元の時間に戻る

--- main.js
+++ main.js
@@ -3,11 +3,12 @@
 {
   function check() {
     // 残り時間 = 終了時刻 - 現在時刻
-    const countdown = endTime - new Date().getTime();
+    let countdown = endTime - new Date().getTime();
 
     // タイマーの終了
     if (countdown < 0) {
       clearInterval(intervalId);
+      countdown = 3 * 1000;
     }
 
     timer.textContent = countdown;
