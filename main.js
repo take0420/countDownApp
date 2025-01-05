89e0b79230b0f1744ab87ee0da3b48c324b49761
take0420 <zakisen81@gmail.com>
2025-01-05 11:31:15 +0900

    feat:

--- main.js
+++ main.js
@@ -5,18 +5,24 @@
     // 残り時間 = 終了時刻 - 現在時刻
     const countdown = endTime - new Date().getTime();
 
+    // タイマーの終了
+    if (countdown < 0) {
+      clearInterval(intervalId);
+    }
+
     timer.textContent = countdown;
   }
 
   const timer = document.getElementById('timer');
   const btn = document.getElementById('btn');
   let endTime;
+  let intervalId;
 
   // タイマーの終了時刻を求める
   btn.addEventListener('click', () => {
     endTime = new Date().getTime() + 3 * 1000;
 
     // 残り時間を表示する
-    setInterval(check, 100);
+    intervalId = setInterval(check, 100);
   });
 }
